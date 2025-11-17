1e724af98178557476933dd29898b80a2942472c
Savani <163087650+Dax-savani@users.noreply.github.com>
2025-11-17 16:09:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 const port = process.env.PORT || 9000;
 
-// Initialize DB poolllllll
+// Initialize DB poollllll
 const pool = createPool();
 
 // Health/status endpoint
